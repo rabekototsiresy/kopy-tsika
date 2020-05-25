cc67e1aac861457a5096738f5c684065ba9f22b1
Tsiresy <tsiresyrabekoto@gmail.com>
2020-05-25 20:46:25 +0200

    display employe list

--- src/Components/Employe/ListEmploye/index.js
+++ src/Components/Employe/ListEmploye/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useContext, useState, useEffect } from 'react'
 import TextField from '@material-ui/core/TextField';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
@@ -12,74 +12,104 @@ import EditOutlinedIcon from '@material-ui/icons/EditOutlined';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import IconButton from '@material-ui/core/IconButton';
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import { FirebaseContext } from '../../Firebase'
+import Alert from '@material-ui/lab/Alert';
 const useStyles = makeStyles((theme) => ({
   main: {
     padding: '100px',
-    
 
   },
 
-  
 }));
 
 
 const ListEmploye = () => {
   const classes = useStyles();
+  const firebase = useContext(FirebaseContext)
+  const [listEmploye, setListEmploye] = useState([])
+  const [data, setData] = useState('')
+
+  useEffect(() => {
+    firebase.getUser("Employe")
+      .then((collection) => {
+        if (collection) {
+          let tabTemp = []
+          collection.docs.map(doc => {
+            tabTemp.push({ info: doc.data(), idDoc: doc.id })
+          })
+
+          setListEmploye(tabTemp)
+        } else {
+          // setError(true)
+          console.log(" Collection not foud une erreur est survenu")
+        }
+      })
+      .catch(err => {
+        console.log(" une erreur est survenu")
+      })
+  }, [listEmploye])
+
+  const displayListEmploye = listEmploye.length !== 0 ? (
+    listEmploye.map((user, index) => {
+      return (
+        <TableRow>
+          <TableCell >{user.info.fullName}</TableCell>
+          <TableCell >{user.info.username}</TableCell>
+          <TableCell >
+            <Link to={`/employe/update-employe/${user.idDoc}`}>
+              <IconButton color="primary" size="small" aria-label="add to shopping cart">
+                <EditOutlinedIcon />
+              </IconButton>
+            </Link>
+          </TableCell>
+        </TableRow>
+      )
+    })
+
+  )
+    :
+    (
+      <TableRow>
+        <TableCell >
+          Employé non disponible
+            </TableCell>
+            <TableCell >
+            </TableCell>
+            <TableCell >
+            </TableCell>
+      </TableRow>
+    )
+
   return (
-   
-    <Grid item md={12} style={{padding: '20px'}}>
-    <h1>Liste Employe</h1>
-    <Divider />
-    
-    <Divider />
-    <Grid item md={12}>
-      <Table className={classes.table} aria-label="simple table">
-      <TableHead>
-          <TableRow>
 
-            <TableCell >ID </TableCell>
-            <TableCell >NOM</TableCell>
-            <TableCell>PSEUDO</TableCell>
-            <TableCell>EDITER</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
+    <Grid item md={12} style={{ padding: '20px' }}>
+      <h1>Liste Employe</h1>
+      <Divider />
+
+      <Divider />
+      <Grid item md={12}>
+        <Table className={classes.table} aria-label="simple table">
+          <TableHead>
             <TableRow>
-            <TableCell >XS5D</TableCell>
-            <TableCell >RABEKOTO TSIRESY NY RIANA</TableCell>
-            <TableCell >tsiresy</TableCell>
-            <TableCell >
-             <Link to="/employe/update-civil">
-             <IconButton color="primary" size="small"  aria-label="add to shopping cart">
-              <EditOutlinedIcon />
-                </IconButton>
-             </Link>
-            </TableCell>
-             
+
+              <TableCell >NOM COMPLET</TableCell>
+              <TableCell>PSEUDO</TableCell>
+              <TableCell>EDITER</TableCell>
             </TableRow>
-            <TableRow>
-            <TableCell >XS5D</TableCell>
-            <TableCell >RABEKOTO TSIRESY NY RIANA</TableCell>
-            <TableCell >tsiresy</TableCell>
-            <TableCell >
-             <Link to="/employe/update-civil">
-             <IconButton color="primary" size="small"  aria-label="add to shopping cart">
-              <EditOutlinedIcon />
-                </IconButton>
-             </Link>
-            </TableCell>
-             
-            </TableRow>
-       
-        </TableBody>
-      </Table>
+          </TableHead>
+          <TableBody>
+
+            {displayListEmploye}
+
+          </TableBody>
+        </Table>
       </Grid>
-  </Grid>
+    </Grid>
 
-      
-    
+
+
   )
 }
 
-export default ListEmploye
+export default React.memo(ListEmploye)
